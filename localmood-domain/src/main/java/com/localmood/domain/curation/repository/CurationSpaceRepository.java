3577a3adb03d1b953927c75414b91fb058c81312
yeni-choi <yewonchoi@ewhain.net>
2024-01-17 00:36:21 +0900

    :sparkles: feat: Add queries for getting curation images in Repositories feat: Add findSpaceIdsByCurationId in CurationSpaceRepository feat: Add findTop5ImageUrlsBySpaceIds in ReviewImgRepository
    Related to: #6

--- localmood-domain/src/main/java/com/localmood/domain/curation/repository/CurationSpaceRepository.java
+++ localmood-domain/src/main/java/com/localmood/domain/curation/repository/CurationSpaceRepository.java
@@ -1,10 +1,19 @@
 package com.localmood.domain.curation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.localmood.domain.curation.entity.CurationSpace;
 
 @Repository
 public interface CurationSpaceRepository extends JpaRepository<CurationSpace, Long> {
+	int countByCurationId(Long curationId);
+
+	// Curation ID에 속한 Space ID 가져오기
+	@Query("SELECT cs.space.id FROM CurationSpace cs WHERE cs.curation.id = :curationId")
+	List<Long> findSpaceIdsByCurationId(@Param("curationId") Long curationId);
 }
