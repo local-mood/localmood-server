feb07f976300f967e51ae2d5640a550718cc67db
yeni-choi <yewonchoi@ewhain.net>
2024-01-16 23:34:56 +0900

    :sparkles: feat: Add random curation query in CurationRepository
    Related to: #6

--- localmood-domain/src/main/java/com/localmood/domain/curation/repository/CurationRepository.java
+++ localmood-domain/src/main/java/com/localmood/domain/curation/repository/CurationRepository.java
@@ -1,10 +1,15 @@
 package com.localmood.domain.curation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.localmood.domain.curation.entity.Curation;
 
 @Repository
 public interface CurationRepository extends JpaRepository<Curation, Long> {
+	@Query(value = "SELECT id FROM curation ORDER BY RAND() LIMIT 5", nativeQuery = true)
+	List<Long> findRandomCurationIds();
 }
