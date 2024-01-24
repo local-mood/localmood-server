6ad64489ced2a2aded2ff0200d20f11273425828
yeni-choi <yewonchoi@ewhain.net>
2024-01-24 15:04:54 +0900

    :sparkles: feat: Change API response format in getCurationsForMember
    feat: Add ID field & toMap method in CurationResponseDto
    feat: Change response of getCurationsForMember in CurationController
    feat: Change logic of getCurationsForMember in CurationService
    
    Related to: #24

--- localmood-api/src/main/java/com/localmood/curation/response/CurationResponseDto.java
+++ localmood-api/src/main/java/com/localmood/curation/response/CurationResponseDto.java
@@ -1,6 +1,8 @@
 package com.localmood.curation.response;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,18 +11,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CurationResponseDto {
 
+	private Long id;
 	private String author;
 	private List<String> image;
 	private String title;
 	private int spaceCount;
 	private List<String> keyword;
 
-	public CurationResponseDto(String authorName, List<String> image, String title, int spaceCount,
+	public CurationResponseDto(Long id, String authorName, List<String> image, String title, int spaceCount,
 		List<String> keyword) {
+		this.id = id;
 		this.author = authorName;
 		this.image = image;
 		this.title = title;
 		this.spaceCount = spaceCount;
 		this.keyword = keyword;
+	}
+
+	public Map<String, Object> toMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", this.id);
+		map.put("author", this.author);
+		map.put("image", this.image);
+		map.put("title", this.title);
+		map.put("spaceCount", this.spaceCount);
+		map.put("keyword", this.keyword);
+		return map;
 	}
 }
