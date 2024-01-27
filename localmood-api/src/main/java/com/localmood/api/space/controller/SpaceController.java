0184eb74fa93f3543beb80b8718185bc5adbbda9
gkim <kimkm0419@naver.com>
2024-01-27 21:46:34 +0900

    :art: fix: Fix url format

--- localmood-api/src/main/java/com/localmood/api/space/controller/SpaceController.java
+++ localmood-api/src/main/java/com/localmood/api/space/controller/SpaceController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/spaces")
+@RequestMapping("/api/v1/spaces")
 public class SpaceController {
 
 	private final SpaceService spaceService;
