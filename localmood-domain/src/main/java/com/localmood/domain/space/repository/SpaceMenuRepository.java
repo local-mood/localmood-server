f7c392e7e9a196f23ff7d23642650093d8fc0f3d
gkim <kimkm0419@naver.com>
2024-01-28 04:16:49 +0900

    :sparkles: feat: Create getSpaceDetail API
    Related to: #62

--- localmood-domain/src/main/java/com/localmood/domain/space/repository/SpaceMenuRepository.java
+++ localmood-domain/src/main/java/com/localmood/domain/space/repository/SpaceMenuRepository.java
@@ -1,5 +1,7 @@
 package com.localmood.domain.space.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.localmood.domain.space.entity.SpaceMenu;
 
 @Repository
 public interface SpaceMenuRepository extends JpaRepository<SpaceMenu, Long> {
+	Optional<SpaceMenu> findBySpaceId(Long spaceId);
 }
--- localmood-domain/src/main/java/com/localmood/domain/space/service/SpaceService.java
+++ localmood-domain/src/main/java/com/localmood/domain/space/service/SpaceService.java
@@ -8,12 +8,19 @@ import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 
+import com.localmood.domain.review.repository.ReviewImgRepository;
 import com.localmood.domain.scrap.entity.ScrapSpace;
 import com.localmood.domain.scrap.repository.ScrapSpaceRepository;
+import com.localmood.domain.space.dto.SpaceDetailDto;
 import com.localmood.domain.space.dto.SpaceDto;
 import com.localmood.domain.space.dto.SpaceRecommendDto;
 import com.localmood.domain.space.dto.request.SpaceFilterRequest;
 import com.localmood.domain.space.dto.request.SpaceSearchRequest;
+import com.localmood.domain.space.entity.Space;
+import com.localmood.domain.space.entity.SpaceInfo;
+import com.localmood.domain.space.entity.SpaceMenu;
+import com.localmood.domain.space.repository.SpaceInfoRepository;
+import com.localmood.domain.space.repository.SpaceMenuRepository;
 import com.localmood.domain.space.repository.SpaceRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +30,10 @@ import lombok.RequiredArgsConstructor;
 public class SpaceService {
 
 	private final SpaceRepository spaceRepository;
+	private final SpaceInfoRepository spaceInfoRepository;
+	private final SpaceMenuRepository spaceMenuRepository;
+	private final ReviewImgRepository reviewImgRepository;
+	private final ScrapSpaceRepository scrapSpaceRepository;
 
 	public Map<String, List<SpaceRecommendDto>> getSpaceRecommendList(Long memberId) {
 		String[] keywordArr = {"연인과의 데이트", "친구와의 만남", "왁자지껄 떠들기 좋은", "대화에 집중할 수 있는"};
@@ -61,5 +72,40 @@ public class SpaceService {
 				sort,
 				memberId
 		);
+	}
+
+	public HashMap<String,Object> getSpaceDetail(Long spaceId, Long memberId) {
+		HashMap<String, Object> spaceDetailMap = new HashMap<>();
+
+		Space space = spaceRepository.findById(spaceId).orElseThrow();
+		SpaceInfo spaceInfo = spaceInfoRepository.findBySpaceId(spaceId).orElseThrow();
+		SpaceMenu spaceMenu = spaceMenuRepository.findBySpaceId(spaceId).orElseThrow();
+		List<String> imgUrlList = reviewImgRepository.findImageUrlsBySpaceId(spaceId);
+		Boolean isScraped = scrapSpaceRepository.existsByMemberIdAndSpaceId(memberId, spaceId);
+
+		spaceDetailMap.put("info",
+				SpaceDetailDto.builder()
+						.id(space.getId())
+						.name(space.getName())
+						.imgUrlList(imgUrlList)
+						.address(space.getAddress())
+						.type(space.getType())
+						.subType(space.getSubType())
+						.dish(spaceMenu.getDish())
+						.dishDesc(spaceMenu.getDishDesc())
+						.purpose(spaceInfo.getPurpose())
+						.mood(spaceInfo.getMood())
+						.music(spaceInfo.getMusic())
+						.positiveEval(spaceInfo.getPositiveEval())
+						.negativeEval(spaceInfo.getNegativeEval())
+						.isScraped(isScraped)
+						.build()
+		);
+
+		// 공간과 비슷한 장소
+
+		// 공간이 담긴 큐레이션
+
+		return spaceDetailMap;
 	}
 }
