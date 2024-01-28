8f8df2846c4cccad2cb005f9e553f5d3c275510d
gkim <kimkm0419@naver.com>
2024-01-28 15:57:52 +0900

    :sparkles: feat: Create auth related APIs (missing files)
    Related to: #36

--- localmood-core/src/main/java/com/localmood/common/exception/ErrorCode.java
+++ localmood-core/src/main/java/com/localmood/common/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버를 찾지 못했습니다.", "존재하는 멤버인지 확인해주세요."),
+	ALREADY_MEMBER_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.", "다른 이메일을 사용해주세요."),
 	CURATION_NOT_FOUND(HttpStatus.NOT_FOUND, "큐레이션을 찾지 못했습니다.", "존재하는 큐레이션인지 확인해주세요."),
 	SPACE_NOT_FOUND(HttpStatus.NOT_FOUND, "공간을 찾지 못했습니다.", "존재하는 공간인지 확인해주세요."),
 	SPACE_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "공간 정보를 찾지 못했습니다.", "존재하는 공간 정보인지 확인해주세요."),
