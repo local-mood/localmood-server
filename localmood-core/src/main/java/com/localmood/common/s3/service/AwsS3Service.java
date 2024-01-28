13c9476d225efbac4b0c606d8d8ad9551dd71ff6
yeni-choi <yewonchoi@ewhain.net>
2024-01-28 23:10:39 +0900

    :sparkles: feat: Add generateFileUrl in AwsS3Service
    Related to: #86

--- localmood-core/src/main/java/com/localmood/common/s3/service/AwsS3Service.java
+++ localmood-core/src/main/java/com/localmood/common/s3/service/AwsS3Service.java
@@ -1,24 +1,20 @@
 package com.localmood.common.s3.service;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.localmood.common.exception.ErrorCode;
+import com.localmood.common.exception.LocalmoodException;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-
 
 @Service
 @RequiredArgsConstructor
@@ -29,24 +25,24 @@ public class AwsS3Service {
 	private final AmazonS3 amazonS3;
 
 	public String uploadFile(MultipartFile file) {
-		String fileName = createFileName(file.getOriginalFilename());
-		ObjectMetadata objectMetadata = new ObjectMetadata();
-		objectMetadata.setContentLength(file.getSize());
-		objectMetadata.setContentType(file.getContentType());
+		try {
+			String fileName = generateFileName(file);
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentLength(file.getSize());
+			metadata.setContentType(file.getContentType());
+			amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
 
-		try (InputStream inputStream = file.getInputStream()) {
-			amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-				.withCannedAcl(CannedAccessControlList.PublicRead));
+			// 업로드된 파일 URL 생성
+			String fileUrl = generateFileUrl(fileName);
+			return fileUrl;
 		} catch (IOException e) {
-			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+			throw new LocalmoodException(ErrorCode.FILE_UPLOAD_FAILED);
 		}
-
-		return fileName;
 	}
 
-	// 파일 업로드시 파일명 난수화
-	private String createFileName(String fileName) {
-		return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+	// 파일명 생성
+	private String generateFileName(MultipartFile file) {
+		return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
 	}
 
 	// 파일 타입과 상관없이 업로드할 수 있게 하도록 "." 존재 유무만 판단
@@ -57,4 +53,10 @@ public class AwsS3Service {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
 		}
 	}
+
+	// 파일 url 생성
+	private String generateFileUrl(String fileName) {
+		return bucket + ".s3.amazonaws.com/" + fileName;
+	}
 }
+
