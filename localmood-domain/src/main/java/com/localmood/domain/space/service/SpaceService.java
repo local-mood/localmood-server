cc7ca8700b52eded2f624e4649304bb203f87e66
gkim <kimkm0419@naver.com>
2024-01-29 07:51:03 +0900

    :recycle: refactor: Fix space related API available of unauthorized users
    Related to: #102

--- localmood-domain/src/main/java/com/localmood/domain/space/service/SpaceService.java
+++ localmood-domain/src/main/java/com/localmood/domain/space/service/SpaceService.java
@@ -3,12 +3,14 @@ package com.localmood.domain.space.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 
 import com.localmood.domain.curation.repository.CurationRepository;
+import com.localmood.domain.member.entity.Member;
 import com.localmood.domain.review.repository.ReviewImgRepository;
 import com.localmood.domain.scrap.repository.ScrapSpaceRepository;
 import com.localmood.domain.space.dto.SpaceDetailDto;
@@ -38,13 +40,13 @@ public class SpaceService {
 	private final ScrapSpaceRepository scrapSpaceRepository;
 	private final CurationRepository curationRepository;
 
-	public Map<String, List<SpaceRecommendDto>> getSpaceRecommendList(Long memberId) {
+	public Map<String, List<SpaceRecommendDto>> getSpaceRecommendList(Optional<Member> member) {
 		String[] keywordArr = {"연인과의 데이트", "친구와의 만남", "왁자지껄 떠들기 좋은", "대화에 집중할 수 있는"};
 		HashMap<String, List<SpaceRecommendDto>> spaceRecommendListMap = new HashMap<>();
 
 		for (int i=0; i < keywordArr.length; i++) {
-			var restaurantList = spaceRepository.findRestaurantRecommendByKeyword(keywordArr[i], memberId);
-			var cafeList = spaceRepository.findCafeRecommendByKeyword(keywordArr[i], memberId);
+			var restaurantList = spaceRepository.findRestaurantRecommendByKeyword(keywordArr[i], member);
+			var cafeList = spaceRepository.findCafeRecommendByKeyword(keywordArr[i], member);
 
 			var mergedSpaceList = Stream.of(restaurantList, cafeList)
 					.flatMap(x -> x.stream())
@@ -56,11 +58,11 @@ public class SpaceService {
 		return spaceRecommendListMap;
 	}
 
-	public List<SpaceSearchDto> getSpaceSearchList(SpaceSearchRequest request, String sort, Long memberId) {
-		return spaceRepository.findSpaceByName(request.getName(), sort, memberId);
+	public List<SpaceSearchDto> getSpaceSearchList(SpaceSearchRequest request, String sort, Optional<Member> member) {
+		return spaceRepository.findSpaceByName(request.getName(), sort, member);
 	}
 
-	public List<SpaceSearchDto> getSpaceFilterList(SpaceFilterRequest request, String sort, Long memberId) {
+	public List<SpaceSearchDto> getSpaceFilterList(SpaceFilterRequest request, String sort, Optional<Member> member) {
 		return spaceRepository.findSpaceByKeywords(
 				request.getType(),
 				request.getSubType(),
@@ -73,18 +75,18 @@ public class SpaceService {
 				request.getDish(),
 				request.getDisDesc(),
 				sort,
-				memberId
+				member
 		);
 	}
 
-	public HashMap<String,Object> getSpaceDetail(Long spaceId, Long memberId) {
+	public HashMap<String,Object> getSpaceDetail(Long spaceId, Optional<Member> member) {
 		HashMap<String, Object> spaceDetailMap = new HashMap<>();
 
 		Space space = spaceRepository.findById(spaceId).orElseThrow();
 		SpaceInfo spaceInfo = spaceInfoRepository.findBySpaceId(spaceId).orElseThrow();
 		SpaceMenu spaceMenu = spaceMenuRepository.findBySpaceId(spaceId).orElseThrow();
 		List<String> imgUrlList = reviewImgRepository.findImageUrlsBySpaceId(spaceId);
-		Boolean isScraped = scrapSpaceRepository.existsByMemberIdAndSpaceId(memberId, spaceId);
+		Boolean isScraped = member.isPresent() ? scrapSpaceRepository.existsByMemberIdAndSpaceId(member.get().getId(), spaceId) : false;
 
 		spaceDetailMap.put("info",
 				SpaceDetailDto.builder()
@@ -108,10 +110,10 @@ public class SpaceService {
 		);
 
 		// 공간과 비슷한 장소
-		spaceDetailMap.put("similarSpaceList", spaceRepository.findSimilarSpace(spaceInfo.getPurpose(), spaceInfo.getMood(), memberId));
+		spaceDetailMap.put("similarSpaceList", spaceRepository.findSimilarSpace(spaceInfo.getPurpose(), spaceInfo.getMood(), member));
 
 				// 공간이 담긴 큐레이션
-		spaceDetailMap.put("relatedCurationList", curationRepository.findCurationBySpaceId(spaceId, memberId));
+		spaceDetailMap.put("relatedCurationList", curationRepository.findCurationBySpaceId(spaceId, member));
 
 		return spaceDetailMap;
 	}
