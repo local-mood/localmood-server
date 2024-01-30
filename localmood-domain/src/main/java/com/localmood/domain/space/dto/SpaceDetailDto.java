d18541e462fff55bb32c99bef8443a2d75cbcc1a
gkim <kimkm0419@naver.com>
2024-01-31 00:15:30 +0900

    :recycle: refactor: Change to SpaceDetailDto to allow certain null values
    Related to: #126

--- localmood-domain/src/main/java/com/localmood/domain/space/dto/SpaceDetailDto.java
+++ localmood-domain/src/main/java/com/localmood/domain/space/dto/SpaceDetailDto.java
@@ -1,6 +1,7 @@
 package com.localmood.domain.space.dto;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.localmood.common.utils.ArrayUtil;
 import com.localmood.domain.space.entity.SpaceDish;
@@ -20,35 +21,37 @@ public class SpaceDetailDto {
 	private List<String> imgUrlList;
 	private String address;
 	private SpaceType type;
-	private String subType;
-	private String dish;
+	private Optional<SpaceSubType> subType;
+	private Optional<SpaceDish> dish;
 	private String dishDesc;
 	private String visitorNum;
 	private String optionalService;
 	private List<String> purpose;
 	private String mood;
 	private String music;
-	private String[][] positiveEval;
-	private String[][] negativeEval;
+	private Optional<String[][]> positiveEval;
+	private Optional<String[][]> negativeEval;
 	private Boolean isScraped;
 
 	@Builder
-	public SpaceDetailDto(Long id, String name, List<String> imgUrlList, String address, SpaceType type, SpaceSubType subType, SpaceDish dish, String dishDesc, String visitorNum, String optionalService, String purpose, String mood, String music, String positiveEval, String negativeEval, Boolean isScraped){
+	public SpaceDetailDto(Long id, String name, List<String> imgUrlList, String address, SpaceType type, Optional<SpaceSubType> subType, Optional<SpaceDish> dish, String dishDesc, String visitorNum, String optionalService, String purpose, String mood, String music, Optional<String> positiveEval, Optional<String> negativeEval, Boolean isScraped){
 		this.id = id;
 		this.name = name;
 		this.imgUrlList = imgUrlList;
 		this.address = address;
 		this.type = type;
-		this.subType = subType.getValue();
-		this.dish = dish.getValue();
+		this.subType = subType;
+		this.dish = dish;
 		this.dishDesc = dishDesc;
 		this.visitorNum = visitorNum;
 		this.optionalService = optionalService;
 		this.purpose = ArrayUtil.toArr(purpose);
 		this.mood = mood;
 		this.music = music;
-		this.positiveEval = ArrayUtil.to2DArr(positiveEval);
-		this.negativeEval = ArrayUtil.to2DArr(negativeEval);
+		this.positiveEval = positiveEval.isEmpty() ? null :
+				Optional.of(ArrayUtil.to2DArr(String.valueOf(positiveEval)));
+		this.negativeEval = negativeEval.isEmpty() ? null :
+				Optional.of(ArrayUtil.to2DArr(String.valueOf(negativeEval)));
 		this.isScraped = isScraped;
 	}
 }
