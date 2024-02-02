408783a79c08575db02aea2be84ed3e56c213bef
gkim <kimkm0419@naver.com>
2024-02-02 09:22:57 +0900

    :wrench: fix: Fix S3 Config format
    Related to: #183

--- localmood-core/src/main/java/com/localmood/common/config/S3Config.java
+++ localmood-core/src/main/java/com/localmood/common/config/S3Config.java
@@ -10,6 +10,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 
 @Slf4j
 @Configuration
@@ -25,13 +26,19 @@ public class S3Config {
 	private String region;
 
 	@Bean
+	@Primary
+	public BasicAWSCredentials awsCredentialsProvider(){
+		BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+		return basicAWSCredentials;
+	}
+
+	@Bean
 	public AmazonS3Client amazonS3Client() {
-		BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
 		return (AmazonS3Client) AmazonS3ClientBuilder
 			.standard()
 			.withRegion(region)
-			.withCredentials(new AWSStaticCredentialsProvider(credentials))
+			.withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
 			.build();
 	}
 }
