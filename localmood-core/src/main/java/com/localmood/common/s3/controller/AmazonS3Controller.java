3299c10bc354c23775178d45f1e9842ac4e7c889
yeni-choi <yewonchoi@ewhain.net>
2024-02-02 14:24:56 +0900

    :recycle: refactor: Change into RequestParam
    Related to: #183

--- localmood-core/src/main/java/com/localmood/common/s3/controller/AmazonS3Controller.java
+++ localmood-core/src/main/java/com/localmood/common/s3/controller/AmazonS3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,7 +27,7 @@ public class AmazonS3Controller {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
 	public ResponseEntity<List<String>> uploadFile(
-		@RequestPart(value = "file", required = true) MultipartFile[] multipartFiles) {
+		@RequestParam(value = "file", required = true) MultipartFile[] multipartFiles) {
 		List<String> uploadedFileUrls = new ArrayList<>();
 
 		for (MultipartFile file : multipartFiles) {
