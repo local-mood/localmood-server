a8a61285ae003f88c236d46b61f1d06d0b84dbf8
yeni-choi <yewonchoi@ewhain.net>
2024-02-27 17:20:09 +0900

    :sparkles: feat: Create getSpaceScrap API
    feat: Create getSpaceScrap in SpaceScrapController
    feat: Create getSpaceScrap in SpaceScrapService
    
    Related to: #210

---
+++ localmood-domain/src/main/java/com/localmood/domain/space/service/SpaceScrapService.java
@@ -0,0 +1,31 @@
+package com.localmood.domain.space.service;
+
+import com.localmood.domain.member.entity.Member;
+import com.localmood.domain.scrap.repository.ScrapSpaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SpaceScrapService {
+
+    private final ScrapSpaceRepository scrapSpaceRepository;
+
+    public Map<String, Boolean> getSpaceScrap(Long spaceId, Optional<Member> memberOptional) {
+        Map<String, Boolean> response = new HashMap<>();
+        boolean isScrapped = false;
+
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            isScrapped = scrapSpaceRepository.existsByMemberIdAndSpaceId(member.getId(), spaceId);
+        }
+
+        response.put("isScrapped", isScrapped);
+        return response;
+    }
+
+}
