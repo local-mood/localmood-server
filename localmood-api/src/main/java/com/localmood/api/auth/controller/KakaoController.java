f1aab748c2ba11eccb7453655c3c86a0f7d4b6a1
yeni-choi <yewonchoi@ewhain.net>
2024-03-04 18:22:00 +0900

    :sparkles: feat: Create get kakao access token logic
    feat: Create get token logic in KakaoController
    feat: Create KakoTokenJsonData & KakoTokenResponse
    
    Related to: #222

--- localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
+++ localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
@@ -1,18 +1,32 @@
 package com.localmood.api.auth.controller;
 
+import com.localmood.api.auth.dto.LoginRequestDto;
+import com.localmood.api.auth.dto.oauth2.token.KakaoTokenJsonData;
+import com.localmood.api.auth.dto.oauth2.token.KakaoTokenResponse;
+import com.localmood.api.auth.dto.oauth2.user.KakaoUserInfoDto;
+import com.localmood.api.auth.jwt.entity.TokenDto;
+import com.localmood.api.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Description;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Tag(name = "Kakao Auth", description = "카카오 인증/인가 API")
 @Controller
 @RequestMapping("/api/v1/auth/kakao")
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoController {
+
+    private final KakaoTokenJsonData kakaoTokenJsonData;
+    private final AuthService authService;
 
     @Value("${oauth.redirect-uri}") String REDIRECT_URI;
     @Value("${oauth.client-id}") String CLIENT_ID;
@@ -26,4 +40,16 @@ public class KakaoController {
         return "loginForm";
     }
 
+    @Description("카카오 로그인을 마치면 자동으로 실행됩니다. 인가 코드를 이용해 토큰을 받고, 해당 토큰으로 사용자 정보를 조회합니다.")
+    @GetMapping("")
+    @ResponseBody
+    public ResponseEntity<?> kakaoOauth(@RequestParam("code") String code) throws IOException {
+        // 인가 코드로 Access Token 받아오기
+        KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
+
+
+        return ResponseEntity
+                .ok()
+                .build();
+    }
 }
