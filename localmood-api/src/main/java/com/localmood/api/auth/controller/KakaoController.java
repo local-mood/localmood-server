03b31a17217841b0bf9e8d300a626197abadaf22
yeni-choi <yewonchoi@ewhain.net>
2024-03-08 00:05:39 +0900

    :fire: chore: remove get kakao loginForm
    refactor: remove get kakao LoginForm in KakaoController
    refactor: remove loginForm
    
    Related to: #222

--- localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
+++ localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
@@ -28,18 +28,6 @@ public class KakaoController {
     private final KakaoTokenJsonData kakaoTokenJsonData;
     private final AuthService authService;
 
-    @Value("${oauth.redirect-uri}") String REDIRECT_URI;
-    @Value("${oauth.client-id}") String CLIENT_ID;
-
-    // TODO: 배포 서버에서 실행 확인 후 삭제 예정
-    @Description("카카오 로그인 폼을 호출합니다.")
-    @GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("redirect_uri", REDIRECT_URI);
-        model.addAttribute("client_id", CLIENT_ID);
-        return "loginForm";
-    }
-
     @Description("카카오 로그인을 마치면 자동으로 실행됩니다. 인가 코드를 이용해 토큰을 받고, 해당 토큰으로 사용자 정보를 조회합니다.")
     @GetMapping("/login")
     @ResponseBody
