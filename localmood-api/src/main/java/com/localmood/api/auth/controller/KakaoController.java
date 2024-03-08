f9e4dc52a7e8371f5e3a3476cd5b8e44f370a9fd
yeni-choi <yewonchoi@ewhain.net>
2024-03-08 15:15:54 +0900

    :sparkles: feat: Add kakao code authorization
    Related to: #222

--- localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
+++ localmood-api/src/main/java/com/localmood/api/auth/controller/KakaoController.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Tag(name = "Kakao Auth", description = "카카오 인증/인가 API")
 @Controller
@@ -25,11 +27,21 @@ public class KakaoController {
 
     private final KakaoTokenJsonData kakaoTokenJsonData;
     private final AuthService authService;
+    private final Set<String> usedAuthorizationCodes = new HashSet<>();
+
 
     @Description("카카오 로그인을 마치면 자동으로 실행됩니다. 인가 코드를 이용해 토큰을 받고, 해당 토큰으로 사용자 정보를 조회합니다.")
     @GetMapping("/login")
     @ResponseBody
     public ResponseEntity<?> kakaoOauth(@RequestParam("code") String code) throws IOException {
+        // 인가 코드 유효성 확인
+        if (usedAuthorizationCodes.contains(code)) {
+            return ResponseEntity.badRequest().body("이미 사용된 인가코드입니다.");
+        }
+
+        // 인가 코드를 사용한 경우 Set에 추가
+        usedAuthorizationCodes.add(code);
+
         // 인가 코드로 Access Token 받아오기
         KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
 
