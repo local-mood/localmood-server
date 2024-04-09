fa5f64f69c9b9c0527d297ac4201dcce0fc9116d
yeni-choi <yewonchoi@ewhain.net>
2024-04-09 14:06:37 +0900

    :sparkles: feat: Add existsByMemberIdAndSpaceId in ReviewRepository
    Related to: #273

--- localmood-domain/src/main/java/com/localmood/domain/review/repository/ReviewRepository.java
+++ localmood-domain/src/main/java/com/localmood/domain/review/repository/ReviewRepository.java
@@ -14,4 +14,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 	List<Review> findBySpaceId(Long spaceId);
 
+    boolean existsByMemberIdAndSpaceId(Long memberId, Long spaceId);
 }
