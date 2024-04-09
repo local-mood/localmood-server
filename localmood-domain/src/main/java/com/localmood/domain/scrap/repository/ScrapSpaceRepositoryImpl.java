426d58c6bef3c18b4960fc9f416f2d9c32069743
yeni-choi <yewonchoi@ewhain.net>
2024-04-09 14:13:39 +0900

    :sparkles: feat: Add isReviewed in findMemberScrapSpace
    Related to: #273

--- localmood-domain/src/main/java/com/localmood/domain/scrap/repository/ScrapSpaceRepositoryImpl.java
+++ localmood-domain/src/main/java/com/localmood/domain/scrap/repository/ScrapSpaceRepositoryImpl.java
@@ -4,13 +4,15 @@ import static com.localmood.domain.curation.entity.QCuration.*;
 import static com.localmood.domain.curation.entity.QCurationSpace.*;
 import static com.localmood.domain.space.entity.QSpace.*;
 import static com.localmood.domain.space.entity.QSpaceInfo.*;
-import static com.querydsl.core.types.dsl.Expressions.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import com.localmood.domain.member.dto.QMemberScrapSpaceDto;
 import com.localmood.domain.member.dto.MemberScrapSpaceDto;
+import com.localmood.domain.review.repository.ReviewRepository;
+import com.localmood.domain.space.entity.SpaceType;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class ScrapSpaceRepositoryImpl implements ScrapSpaceRepositoryCustom{
 
 	private final JPAQueryFactory queryFactory;
+	private final ReviewRepository reviewRepository;
 
 	@Override
 	public List<MemberScrapSpaceDto> findMemberScrapSpaceByMemberId(Long memberId) {
@@ -32,16 +35,13 @@ public class ScrapSpaceRepositoryImpl implements ScrapSpaceRepositoryCustom{
 			return Collections.emptyList();
 		}
 
-		return queryFactory
-				.selectDistinct(
-						new QMemberScrapSpaceDto(
-								space.id,
-								space.name,
-								space.type,
-								space.address,
-								spaceInfo.thumbnailImgUrl,
-								TRUE
-						)
+		List<Tuple> queryResult = queryFactory
+				.select(
+						space.id,
+						space.name,
+						space.type,
+						space.address,
+						spaceInfo.thumbnailImgUrl
 				)
 				.from(space)
 				.leftJoin(spaceInfo)
@@ -50,7 +50,19 @@ public class ScrapSpaceRepositoryImpl implements ScrapSpaceRepositoryCustom{
 				.on(space.id.eq(curationSpace.space.id))
 				.where(curationSpace.curation.id.in(curationIds))
 				.orderBy(curationSpace.modifiedAt.desc())
+				.distinct()
 				.fetch();
+
+		return queryResult.stream().map(tuple -> {
+			Long id = tuple.get(space.id);
+			String name = tuple.get(space.name);
+			SpaceType type = tuple.get(space.type);
+			String address = tuple.get(space.address);
+			String thumbnailImgUrl = tuple.get(spaceInfo.thumbnailImgUrl);
+			boolean isReviewed = reviewRepository.existsByMemberIdAndSpaceId(memberId, id);
+
+			return new MemberScrapSpaceDto(id, name, type, address, thumbnailImgUrl, true, isReviewed);
+		}).collect(Collectors.toList());
 	}
 
 }
