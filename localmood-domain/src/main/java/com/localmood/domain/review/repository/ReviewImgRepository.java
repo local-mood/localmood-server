2f127b38e729fce8c77f65815049b5449c360220
yeni-choi <yewonchoi@ewhain.net>
2024-05-28 23:35:21 +0900

    :recycle: refactor: Set limit of review img in get Space Detail API
    feat: Add findImageUrl query in ReviewImgRepository
    refactor: Revise get imgUrlList logic in SpaceService
    
    Related to: #290

--- localmood-domain/src/main/java/com/localmood/domain/review/repository/ReviewImgRepository.java
+++ localmood-domain/src/main/java/com/localmood/domain/review/repository/ReviewImgRepository.java
@@ -12,13 +12,14 @@ import com.localmood.domain.review.entity.ReviewImg;
 
 @Repository
 public interface ReviewImgRepository extends JpaRepository<ReviewImg, Long> {
-	// Space ID에 속한 이미지 최대 5개 가져오기
+	@Query("SELECT new java.lang.String(ri.imgUrl) FROM ReviewImg ri WHERE ri.space.id = :spaceId")
+	List<String> findImageUrlsBySpaceId(@Param("spaceId") Long spaceId);
+
 	@Query("SELECT ri.imgUrl FROM ReviewImg ri WHERE ri.space.id IN :spaceIds")
 	List<String> findTop5ImageUrlsBySpaceIds(@Param("spaceIds") List<Long> spaceIds);
 
-	// Space ID에 속한 모든 이미지 가져오기
-	@Query("SELECT new java.lang.String(ri.imgUrl) FROM ReviewImg ri WHERE ri.space.id = :spaceId")
-	List<String> findImageUrlsBySpaceId(@Param("spaceId") Long spaceId);
+	@Query("SELECT ri.imgUrl FROM ReviewImg ri WHERE ri.space.id = :spaceId ORDER BY ri.createdAt ASC")
+	List<String> findImageUrlsBySpaceIdOrderByCreatedAtAsc(@Param("spaceId") Long spaceId);
 
 	Optional<ReviewImg> findByReviewId(Long reviewId);
 	List<ReviewImg> findListByReviewId(Long reviewId);
